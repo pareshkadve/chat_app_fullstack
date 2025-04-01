c067df994eb9d000e27184ee88a1d3660b61ef50
sakshi171204 <sakshipawar17120@gamil.com>
2025-04-01 17:28:04 +0530

    Project Completed

---
+++ backend/src/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import {app,server} from "./lib/socket.js";
+import {connectDB} from "./lib/db.js";
+
+import path from "path";
+
+dotenv.config()
+
+const PORT =process.env.PORT
+const _dirname=path.resolve();
+
+// ✅ Increase payload size limit
+app.use(express.json({ limit: "50mb" }));  
+app.use(express.urlencoded({ limit: "50mb", extended: true })); app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}
+));
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
+
+server.listen(PORT, () => {
+    console.log("Server is running on port :"+PORT);
+    connectDB()
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react'
+import { Routes ,Route, Navigate} from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import Navbar from './components/Navbar';  // Adjust the path if needed
+import { useAuthStore } from './store/useAuthStore';
+import {Loader} from "lucide-react"
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import { Toaster } from "react-hot-toast";
+import {useThemeStore} from "./store/useThemeStore";
+
+ const App = () => {
+  const {authUser,checkAuth, isCheckingAuth,onlineUsers}= useAuthStore()
+
+const {theme}= useThemeStore()
+console.log({onlineUsers});
+
+  useEffect(()=>{
+    checkAuth()
+  },[checkAuth])
+
+  console.log({authUser});
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
+
+  return (
+  <div data-theme ={theme}>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={authUser ?<HomePage/>:<Navigate to ="/login"/>} />
+        <Route path="/login" element={!authUser ? <LoginPage/>:<Navigate to ="/"/>} />
+        <Route path='/signup' element={!authUser ? <SignUpPage/>:<Navigate to ="/"/>} />
+        <Route path='/profile' element={authUser ? <ProfilePage/>:<Navigate to ="/login"/>} />
+        <Route path='/settings' element={<SettingsPage/>}/>
+      </Routes>
+      <Toaster />
+  </div>
+  );
+}
+
+
+export default App;
